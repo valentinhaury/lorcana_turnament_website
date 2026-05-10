c6820c93969eab10ca41678b2a038d1c5a584344
haury <haury@cip.ifi.lmu.de>
2026-05-10 12:39:22 +0200

    changed color scheme

--- lorcana_turnament_calender/urls.py
+++ lorcana_turnament_calender/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("calender/", views.calender, name="calender"),
+    path("", views.calender, name="turnaments"),
+    path("turnaments/", views.calender, name="turnaments"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
 ]
