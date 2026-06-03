e1dd8c9ac69e62078925b34923e72b8bf45f66d3
haury <haury@cip.ifi.lmu.de>
2026-06-03 14:25:14 +0200

    added background, and changed the design to light instead of dark

--- lorcana_turnament_calender/models.py
+++ lorcana_turnament_calender/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    answer_text = models.CharField(max_length=200)
+    answer_text = models.CharField(max_length=600)
     ranking = models.IntegerField(default=0)
 
     def __str__(self):
