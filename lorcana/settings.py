f83a35fdca8e25357d7bd9be6df1c2e263458ae1
valen <haury@cip.ifi.lmu.de>
2026-08-20 19:45:14 +0200

    allowed render as host

--- lorcana/settings.py
+++ lorcana/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     ".up.railway.app",
-    "127.0.0.1"
+    "127.0.0.1",
+    ".onrender.com"
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
